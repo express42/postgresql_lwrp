e690f297265787a89acb1b915fab31e8f9673113
Nikita Borzykh <sample.n@gmail.com>
2012-10-22 06:51:35 +0400

    Increment version to 0.1.2

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email  "info@express42.com"
 license           "MIT"
 description       "Installs and configures postgresql for clients or servers"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.1.1"
+version           "0.1.2"
 recipe            "postgresql::default", "Includes client recipe"
 recipe            "postgresql::client", "Installs postgresql client packages"
 recipe            "postgresql::server", "Installs postgresql server packages, configures postgresql"
