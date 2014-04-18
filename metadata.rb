f5f63e554bade4509c35e07d5b3e0ae38df8a55a
Nikita Borzykh <sample.n@gmail.com>
2014-04-18 00:12:00 -0700

    Change email in metadata

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name             'postgresql'
 maintainer       'LLC Express 42'
-maintainer_email 'info@express42.com'
+maintainer_email 'cookbooks@express42.com'
 license          'MIT'
 description      'Installs and configures postgresql for clients or servers'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
