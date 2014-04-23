28d95661d765421a705dffdcd6db16abaceb874e
Nikita Borzykh <sample.n@gmail.com>
2014-04-23 00:41:55 -0700

    Add fixtures for user and database create

---
+++ test/fixtures/cookbooks/pgtest/recipes/create_database.rb
@@ -0,0 +1,13 @@
+include_recipe 'postgresql::default'
+
+postgresql_database 'test01' do
+  in_version "9.2"
+  in_cluster "main"
+  owner "test01"
+end
+
+postgresql_database 'test02' do
+  in_version "9.2"
+  in_cluster "main"
+  owner "test02"
+end
