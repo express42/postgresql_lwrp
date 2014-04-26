1adb4d996846013c59035d31f521979a70fa8fa4
Nikita Borzykh <sample.n@gmail.com>
2014-04-26 21:22:23 +0400

    Test with pg 9.1/9.2/9.3

--- test/fixtures/cookbooks/pgtest/recipes/create_database.rb
+++ test/fixtures/cookbooks/pgtest/recipes/create_database.rb
@@ -1,13 +1,13 @@
 include_recipe 'postgresql::default'
 
 postgresql_database 'test01' do
-  in_version '9.2'
+  in_version node['postgresql']['defaults']['server']['version']
   in_cluster 'main'
   owner 'test01'
 end
 
 postgresql_database 'test02' do
-  in_version '9.2'
+  in_version node['postgresql']['defaults']['server']['version']
   in_cluster 'main'
   owner 'test02'
 end
--- test/fixtures/cookbooks/pgtest/recipes/create_user.rb
+++ test/fixtures/cookbooks/pgtest/recipes/create_user.rb
@@ -1,14 +1,14 @@
 include_recipe 'postgresql::default'
 
 postgresql_user 'test01' do
-  in_version '9.2'
+  in_version node['postgresql']['defaults']['server']['version']
   in_cluster 'main'
   unencrypted_password 'test01'
   replication true
 end
 
 postgresql_user 'test02' do
-  in_version '9.2'
+  in_version node['postgresql']['defaults']['server']['version']
   in_cluster 'main'
   unencrypted_password 'test02'
   superuser true
