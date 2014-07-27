c382e6611884972de9064e8279b23864bd9b09f0
Nikita Borzykh <sample.n@gmail.com>
2014-07-27 11:09:34 +0400

    Don't be so quiet

--- script/travis/bundle_install.sh
+++ script/travis/bundle_install.sh
@@ -3,6 +3,6 @@
 curl -o bundle.tgz https://s3.amazonaws.com/$AWS_BUCKET/postgresql-cookbook/bundle.tgz
 tar -xf bundle.tgz
 
-bundle install --path .bundle --quiet --without kitchen_vagrant development
+bundle install --path .bundle --without kitchen_vagrant development
 
 exit 0
